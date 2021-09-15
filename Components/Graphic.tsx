35b72f4ebaa17e1a232c00683e6a53bc6856897b
Matt Montgomery <mmontgomery@deseretdigital.com>
2021-09-15 16:58:30 -0600

    feat: ✨ adds download button

--- Components/Graphic.tsx
+++ Components/Graphic.tsx
@@ -1,4 +1,6 @@
+import { Box, Button, Link } from "@material-ui/core";
 import { CircleConfig } from "konva/lib/shapes/Circle";
+import { LegacyRef, useRef } from "react";
 import {
   Stage,
   Layer,
@@ -77,123 +79,147 @@ export default function Graphic({
 }): React.ReactElement {
   const fullHeight = 150 - radius + players.length * radius * 4.5;
   const fullWidth = radius * 25;
+  const stage = useRef(null);
+
+  function downloadURI(uri: string, name: string) {
+    const link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
   return (
-    <Stage width={fullWidth} height={fullHeight}>
-      <Layer>
-        <Group>
-          <Rect
-            x={0}
-            y={0}
-            height={fullHeight}
-            fill="#f0f0f0"
-            width={fullWidth}
-          />
-        </Group>
-        <Group>
-          <Rect
-            x={25}
-            y={-25}
-            width={fullWidth - 50}
-            height={fullHeight - 50}
-            stroke="#ddd"
-            strokeWidth={8}
-          />
-          <Rect
-            x={radius * 6}
-            y={fullHeight - 200}
-            width={fullWidth - radius * 12}
-            height={125}
-            stroke="#ddd"
-            strokeWidth={8}
-          />
-          <Rect
-            x={radius * 8}
-            y={fullHeight - 150}
-            width={fullWidth - radius * 16}
-            height={75}
-            stroke="#ddd"
-            strokeWidth={8}
-          />
-          <Arc
-            strokeWidth={8}
-            stroke="#ddd"
-            x={fullWidth / 2}
-            y={fullHeight - 200}
-            width={200}
-            angle={180}
-            innerRadius={1}
-            outerRadius={25}
-            rotationDeg={180}
-          />
-          <Line
-            points={[25, fullHeight * 0.2, fullWidth - 25, fullHeight * 0.2]}
-            stroke="#ddd"
-            strokeWidth={8}
-            height={20}
-            width={fullWidth}
-          />
-          <Circle
-            height={150}
-            stroke="#ddd"
-            strokeWidth={8}
-            x={fullWidth / 2}
-            y={fullHeight * 0.2}
-          />
-        </Group>
-        <Group x={25} y={25}>
-          <Text
-            text={matchTitle}
-            fontSize={radius * 0.75}
-            fontStyle="bold"
-            fontFamily="Helvetica Neue"
-            width={fullWidth - 75}
-            align="right"
-          />
-          <Text
-            text={matchDate}
-            fontSize={radius * 0.75}
-            fontFamily="Helvetica Neue"
-            y={radius}
-            width={fullWidth - 75}
-            align="right"
-          />
-          <Text
-            text={subTitle}
-            fontSize={radius * 0.75}
-            fontFamily="Helvetica Neue"
-            y={radius * 2}
-            width={fullWidth - 75}
-            align="right"
-          />
-          <Text
-            text={`lineup.tools.football`}
-            fontSize={radius * 0.5}
-            fontFamily="Helvetica Neue"
-            y={fullHeight - 90}
-            width={fullWidth - 50}
-            align="right"
-          />
-        </Group>
-        <Group x={radius * 4} y={150}>
-          {players.map((group, idx) => (
-            <Group key={idx} y={radius * 4.5 * idx}>
-              {group.map((player, idx) => (
-                <Player
-                  key={idx}
-                  idx={player.idx}
-                  x={getPosition(player.position, radius)}
-                  name={player.name}
-                  playerNumber={player.playerNumber}
-                  radius={radius}
-                  onClick={() => onPlayerClick(player.idx)}
-                  themeColors={themeColors}
-                />
-              ))}
-            </Group>
-          ))}
-        </Group>
-      </Layer>
-    </Stage>
+    <div>
+      <Stage width={fullWidth} height={fullHeight} ref={stage}>
+        <Layer>
+          <Group>
+            <Rect
+              x={0}
+              y={0}
+              height={fullHeight}
+              fill="#f0f0f0"
+              width={fullWidth}
+            />
+          </Group>
+          <Group>
+            <Rect
+              x={25}
+              y={-25}
+              width={fullWidth - 50}
+              height={fullHeight - 50}
+              stroke="#ddd"
+              strokeWidth={8}
+            />
+            <Rect
+              x={radius * 6}
+              y={fullHeight - 200}
+              width={fullWidth - radius * 12}
+              height={125}
+              stroke="#ddd"
+              strokeWidth={8}
+            />
+            <Rect
+              x={radius * 8}
+              y={fullHeight - 150}
+              width={fullWidth - radius * 16}
+              height={75}
+              stroke="#ddd"
+              strokeWidth={8}
+            />
+            <Arc
+              strokeWidth={8}
+              stroke="#ddd"
+              x={fullWidth / 2}
+              y={fullHeight - 200}
+              width={200}
+              angle={180}
+              innerRadius={1}
+              outerRadius={25}
+              rotationDeg={180}
+            />
+            <Line
+              points={[25, fullHeight * 0.2, fullWidth - 25, fullHeight * 0.2]}
+              stroke="#ddd"
+              strokeWidth={8}
+              height={20}
+              width={fullWidth}
+            />
+            <Circle
+              height={150}
+              stroke="#ddd"
+              strokeWidth={8}
+              x={fullWidth / 2}
+              y={fullHeight * 0.2}
+            />
+          </Group>
+          <Group x={25} y={25}>
+            <Text
+              text={matchTitle}
+              fontSize={radius * 0.75}
+              fontStyle="bold"
+              fontFamily="Helvetica Neue"
+              width={fullWidth - 75}
+              align="right"
+            />
+            <Text
+              text={matchDate}
+              fontSize={radius * 0.75}
+              fontFamily="Helvetica Neue"
+              y={radius}
+              width={fullWidth - 75}
+              align="right"
+            />
+            <Text
+              text={subTitle}
+              fontSize={radius * 0.75}
+              fontFamily="Helvetica Neue"
+              y={radius * 2}
+              width={fullWidth - 75}
+              align="right"
+            />
+            <Text
+              text={`lineup.tools.football`}
+              fontSize={radius * 0.5}
+              fontFamily="Helvetica Neue"
+              y={fullHeight - 90}
+              width={fullWidth - 50}
+              align="right"
+            />
+          </Group>
+          <Group x={radius * 4} y={150}>
+            {players.map((group, idx) => (
+              <Group key={idx} y={radius * 4.5 * idx}>
+                {group.map((player, idx) => (
+                  <Player
+                    key={idx}
+                    idx={player.idx}
+                    x={getPosition(player.position, radius)}
+                    name={player.name}
+                    playerNumber={player.playerNumber}
+                    radius={radius}
+                    onClick={() => onPlayerClick(player.idx)}
+                    themeColors={themeColors}
+                  />
+                ))}
+              </Group>
+            ))}
+          </Group>
+        </Layer>
+      </Stage>
+      <Box m={1} style={{ textAlign: "right" }}>
+        <Button
+          onClick={() => {
+            const uri = (stage.current as any)?.toDataURL({
+              pixelRatio: 3,
+            });
+            downloadURI(uri, `lineup-${new Date().getTime()}.png`);
+          }}
+        >
+          Download
+        </Button>
+      </Box>
+    </div>
   );
 }
 
